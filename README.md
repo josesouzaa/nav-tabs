08596035d082ef6e286ea63cb9a6098a3a73bf4a
José de Souza <josee.souzaaa@gmail.com>
2021-05-22 16:14:27 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# Navegação em Tabs com JavaScript
+
+Projeto da aula <a href="https://www.youtube.com/watch?v=sxPEmuW7aUg">Navegação em Tabs com JS | Code/Drops #41</a> da <a href="https://rocketseat.com.br/">Rocketseat</a>.
+
+Projeto realizado com o intuito de praticar a produção de páginas na web, utilizando o JavaScrip para manipulação de elementos.
+
+Me siga no <a href="https://www.linkedin.com/in/jose-de-souza/">Linkedin</a>
---
+++ script.js
@@ -0,0 +1,56 @@
+const $ = document.querySelector.bind(document)
+
+function TabNavigation() {
+  const html = {
+    links: [...$('.tab-links').children],
+    contents: [...$('.tab-content').children],
+    openTab: $('[data-open]')
+  }
+
+  function HideAllTabContent() {
+    html.contents.forEach(section => {
+      section.style.display = 'none'
+    })
+  }
+
+  function removeAllActiveClass() {
+    html.links.forEach(tab => {
+      tab.className = tab.className.replace(' active', '')
+    })
+  }
+
+  function showCurrentTab(id) {
+    const tabcontent = $('#' + id)
+    tabcontent.style.display = 'block'
+  }
+
+  function selectTab(event) {
+    HideAllTabContent()
+    removeAllActiveClass()
+    const target = event.currentTarget
+    showCurrentTab(target.dataset.id)
+    target.className += " active"
+  }
+
+  function listenForChange() {
+    html.links.forEach(tab => {
+      tab.addEventListener('click', selectTab)
+    })
+  }
+
+  function init() {
+    HideAllTabContent()
+    listenForChange()
+    html.openTab.click()
+  }
+
+  return {
+    init
+  }
+
+}
+
+window.addEventListener('load', () => {
+  const tabNavigation = TabNavigation()
+  tabNavigation.init()
+})
